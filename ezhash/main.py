7f4b395873a0c0863bd756c95a897f79cee06dc4
Naffah Abdulla Rasheed <mail@naffah.me>
2022-02-15 19:55:12 +0500

    Added a function to generate and display file hash along with the algorithm used, file path or file hash compared with

--- ezhash/main.py
+++ ezhash/main.py
@@ -11,10 +11,7 @@ app = typer.Typer()
 def main(algorithm: str, path: str, compare_with: Optional[str] = typer.Argument(None)):
     hash = Hash(algorithm, path, compare_with)  # Initialize a Hash object
 
-    if compare_with is None:  # If the file hash to be compared with is not defined
-        typer.secho()
-    else:
-        typer.secho()
+    typer.secho(hash.display_progress())
 
 
 class Hash():
@@ -24,25 +21,37 @@ class Hash():
         self.path = path
         self.compare_with = compare_with
 
-    # Function to generate and return file hash as a string
-    def generate_file_hash(self):
-        if self.algorithm in hashlib.algorithms_available:  # If the user defined algorithm is available
-            # Opens a file, and returns it as a file object
-            with open(self.path, 'rb') as f:
-                algorithm = hashlib.new(self.algorithm)
-                file_size = Path(self.path).stat().st_size
+    """ Function to generate and return file hash as a string """
 
-                with typer.progressbar(length=file_size) as pbar:
-                    while True:
-                        chunk = f.read(8192)
-                        if not chunk:  # If no more bytes to be read
-                            break
-                        algorithm.update(chunk)
-                        pbar.update(len(chunk))
-            return algorithm.hexdigest()  # hexdigest() returns a string
-        raise TypeError(f'Algorithm: {self.algorithm} not supported!')
+    def generate_file_hash(self):
+        if self.algorithm not in hashlib.algorithms_available:  # If the user defined algorithm is not available
+            raise TypeError(f'Algorithm: {self.algorithm} not supported!')
+
+        # Opens a file, and returns it as a file object
+        with open(self.path, 'rb') as f:
+            algorithm = hashlib.new(self.algorithm)
+            file_size = Path(self.path).stat().st_size
+
+            with typer.progressbar(length=file_size) as pbar:
+                while True:
+                    chunk = f.read(8192)
+                    if not chunk:  # If no more bytes to be read
+                        break
+                    algorithm.update(chunk)
+                    pbar.update(len(chunk))
+        return algorithm.hexdigest()  # hexdigest() returns a string
+
+    """ 
+    Calls 'generate_file_hash' function and 
+    displays the file hash along with 
+    algorithm, path or file hash compared with
+    """
 
     def display_progress(self):
-        self.generate_file_hash()
+        dictionary = (vars(self))  # Convert class properties into a dictionary
 
-        for i in range(self):
+        # Iterate over the key value pairs in the dictionary and print them
+        for key, value in dictionary.items():
+            typer.secho(f'{key.upper()}: {value}')
+
+        self.generate_file_hash()
