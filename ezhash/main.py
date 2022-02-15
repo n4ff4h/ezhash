c710d8bbb14903ec141c1a69c93afe567f50878e
Naffah Abdulla Rasheed <mail@naffah.me>
2022-02-15 15:06:58 +0500

    Added some useful comments

--- ezhash/main.py
+++ ezhash/main.py
@@ -9,32 +9,34 @@ app = typer.Typer()
 # Main command
 @app.command()
 def main(algorithm: str, path: str, compare_with: Optional[str] = typer.Argument(None)):
-    hash = Hash(algorithm, path)
+    hash = Hash(algorithm, path)  # Initialize a Hash object
 
-    if compare_with is None:  # if the file hash to be compared with is not defined
+    if compare_with is None:  # If the file hash to be compared with is not defined
         print(hash.generate_file_hash())
     else:
         print()
 
 
-#
 class Hash():
+    # Default constructor
     def __init__(self, algorithm, path):
         self.algorithm = algorithm
         self.path = path
 
+    # Function to generate and return file hash as a string
     def generate_file_hash(self):
         if self.algorithm in hashlib.algorithms_available:  # If the user defined algorithm is available
+            # Opens a file, and returns it as a file object
             with open(self.path, 'rb') as f:
                 algorithm = hashlib.new(self.algorithm)
                 file_size = Path(self.path).stat().st_size
 
-                with typer.progressbar(range(file_size)) as pbar:
+                with typer.progressbar(length=file_size) as pbar:
                     while True:
                         chunk = f.read(8192)
-                        if not chunk:
+                        if not chunk:  # If no more bytes to be read
                             break
                         algorithm.update(chunk)
                         pbar.update(len(chunk))
-            return algorithm.hexdigest()
+            return algorithm.hexdigest()  # hexdigest() returns a string
         raise TypeError(f'Algorithm: {algorithm} not supported!')
